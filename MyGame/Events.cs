6573698349bbe39cab9ca9a1bcd59a4c5f3ee3ed
Infinity <60041234+infinitelyjames@users.noreply.github.com>
2024-10-26 14:02:41 +0100

    Added random event trigger and event controller

--- MyGame/Events.cs
+++ MyGame/Events.cs
@@ -1,18 +1,43 @@
 
 
 using System;
+using System.Collections.Generic;
 
-public class EventDetails {
+public class Event {
     private string title;
     private string description;
     private float probability;
     private Action onEventTriggered;
     
-    public EventDetails(string title, string description, float probability, Action onEventTriggered) {
+    public Event(string title, string description, float probability, Action onEventTriggered) {
         this.title = title;
         this.description = description;
         this.probability = probability;
         this.onEventTriggered = onEventTriggered;
     }
 
+    public void Trigger() {
+        onEventTriggered();
+    }
+
+    public void RandomTrigger() {
+        Random random = new Random();
+        if (random.NextDouble() < probability) {
+            Trigger();
+        }
+    }
+}
+
+public class EventController {
+    private List<Event> events = new List<Event>();
+
+    public EventController(List<Event> events) {
+        this.events = events;
+    }
+
+    public void TriggerRandomEvents() {
+        foreach (Event e in events) {
+            e.RandomTrigger();
+        }
+    }
 }

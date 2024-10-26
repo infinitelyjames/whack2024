ac13375fd0e56aa7b7d845dfcb6e5c2aa99d7ec7
smavanat <edwin.hadass@hotmail.com>
2024-10-26 16:40:21 +0100

    Added account class

---
+++ Source/account.py
@@ -0,0 +1,36 @@
+import pickle
+
+class Account:
+    def __init__(self, name:str, amount:float, interest:float):
+        self.name = name
+        self.amount = amount
+        self.interest = interest
+    def updateInterest():
+        amount *= Account.interest/100
+    def addAmount(change:float):
+        Account.amount += change
+    def removeAmount(change:float):
+        Account.addAmount -= change
+    @staticmethod
+    def loadFromFile(filename:str):
+        with open(filename, "rb") as file:
+            return pickle.load(file)
+    def saveToFile(self, filename:str):
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+class StockAccount:
+    value = 0
+
+    def __init__(self, name:str, shares:int, sharePrice:float):
+        self.name = name
+        self.shares = shares
+        self.sharePrice = sharePrice
+        value = shares*sharePrice
+    @staticmethod
+    def loadFromFile(filename:str):
+        with open(filename, "rb") as file:
+            return pickle.load(file)
+    def saveToFile(self, filename:str):
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
--- Source/player.py
+++ Source/player.py
@@ -1,10 +1,13 @@
 import pickle
+import account
 
 class Player:
     def __init__(self, name:str, startingMoney:float):
         self.name = name
         self.money = startingMoney
         self.startingMoney = startingMoney
+        self.stocks = 0
+        self.accounts = [account.Account("Current Account", startingMoney, 5), account.Account("Savings Account"), 0, 5]
     @staticmethod
     def loadDefaultPlayer(name="Default player"):
         return Player(name, 1000)

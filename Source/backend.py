d4e4fde97b754e3bc859782e9a08ce409311fe15
Infinity <60041234+infinitelyjames@users.noreply.github.com>
2024-10-26 19:14:54 +0100

    backend.py minor changes

--- Source/backend.py
+++ Source/backend.py
@@ -1,11 +1,14 @@
+class Backend:
+    def __init__(self, player):
+        self.player = player
+    
+    # add the backend post request routes to the flask app
+    def addBackendRoutes(app):
+        # add a post request for the /api/changemoney
+        @app.route('/api/changemoney', methods=['POST'])
+        def changeMoney():
+            # placeholder
+            return ""
 
-# add the backend post request routes to the flask app
-def addBackendRoutes(app):
-    # add a post request for the /api/changemoney
-    @app.route('/api/changemoney', methods=['POST'])
-    def changeMoney():
-        # placeholder
-        return ""
-
-    # return the app
-    return app
+        # return the app
+        return app
